9b65aec13d9e2996c427442d5bb26610afb33fc5
bracketsGPE <brackets.gpe@gmail.com>
2021-09-28 14:16:47 +0200

    add save to localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent implements OnInit {
   markdown = ``;
 
   ngOnInit() {
-    localStorage.getItem('markdown');
+    this.markdown = localStorage.getItem('markdown');
   }
 
   saveMarkdown() {
